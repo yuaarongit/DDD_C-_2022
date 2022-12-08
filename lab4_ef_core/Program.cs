5678f067a6b3918e2f831ed1a435f752f5e19254
yuaaron <yuaaronmail@gmail.com>
2022-12-08 16:36:21 +0800

    [modify] 修改最後一筆Blog資料，同時建立新的關聯的Post

--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -2,10 +2,16 @@
 using var db = new BlogContext();
 Console.WriteLine($"資料庫的路徑是:{db.DbPath}");
 
-db.Add(new Blog { Uri = "http://www.uuu.com.tw/" });
-db.Add(new Blog { Uri = "http://www.tibame.com/" });
-db.Add(new Blog { Uri = "http://hahow.in/" });
+// db.Add(new Blog { Uri = "http://www.uuu.com.tw/" });
+// db.Add(new Blog { Uri = "http://www.tibame.com/" });
+// db.Add(new Blog { Uri = "http://hahow.in/" });
+Console.WriteLine("修改一下資料");
+var blog = db.Blogs!.OrderBy(b => b.BlogId).Last();
+blog.Uri = "https://www.cwb.gov.tw";
+blog.Posts.Add(new Post { Title = "urgent", Content = "Type is coming" });
+blog.Posts.Add(new Post { Title = "urgent", Content = "Earthquake last night" });
 db.SaveChanges();
+
 Console.WriteLine("將資料讀回");
 var blogs = db.Blogs!.OrderBy(b => b.BlogId);
 foreach (var b in blogs)
