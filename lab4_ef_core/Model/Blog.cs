1486285e91501d4c9f45f7b1b6f9fd9ba1a8b25b
yuaaron <yuaaronmail@gmail.com>
2022-12-08 15:11:59 +0800

    [add] 增加entity framework的design

---
+++ lab4_ef_core/Model/Blog.cs
@@ -0,0 +1,7 @@
+namespace Model;
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+
+}
---
+++ lab4_ef_core/Model/Post.cs
@@ -0,0 +1,7 @@
+namespace Model;
+public class Post
+{
+    public int PostId { get; set; }
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+}
