4ad21fad0bb74c56acdaabc2bafbe3753cb63693
yuaaron <yuaaronmail@gmail.com>
2022-12-09 16:50:36 +0800

    [add] DependencyInjection

--- lab6_together/Together.Api/Program.cs
+++ lab6_together/Together.Api/Program.cs
@@ -1,9 +1,11 @@
+using Together.Application;
 using Together.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
-    builder.Services.AddScoped<IEventService, EventService>();
+    //builder.Services.AddScoped<IEventService, EventService>();
+    builder.Services.AddApplication();
 }
 
 var app = builder.Build();
