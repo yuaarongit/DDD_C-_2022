f74c3cdb2a6500f7cbbd95cfdf4a0c192155bfa1
yuaaron <yuaaronmail@gmail.com>
2022-12-09 17:05:14 +0800

    [modify] 修改 Program.cs

--- lab6_together/Together.Api/Program.cs
+++ lab6_together/Together.Api/Program.cs
@@ -1,11 +1,13 @@
 using Together.Application;
 using Together.Application.Services;
+using Together.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     //builder.Services.AddScoped<IEventService, EventService>();
     builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
 }
 
 var app = builder.Build();
