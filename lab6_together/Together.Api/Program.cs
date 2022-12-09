2b5bc7edb820d86c283b4a914586ff8d47157547
yuaaron <yuaaronmail@gmail.com>
2022-12-09 16:04:56 +0800

    [modify] add AddScoped

--- lab6_together/Together.Api/Program.cs
+++ lab6_together/Together.Api/Program.cs
@@ -1,13 +1,14 @@
+using Together.Application.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IEventService, EventService>();
 }
 
 var app = builder.Build();
 {
     app.UseHttpsRedirection();
-
     app.MapControllers();
-
     app.Run();
 }
